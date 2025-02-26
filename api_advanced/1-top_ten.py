cc8ba6f374c45b6ed41cf134a31c04154cc219be
David Pontient <d.kayumba1@alustudent.com>
2025-02-26 10:02:18 +0000

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-"""DOCS"""
-import requests
+
+"""
+Displays the titles of 10 hot posts listed for a subreddit
+"""
+
+from requests import get
+import subprocess
 
 
 def top_ten(subreddit):
-    """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    """
+    The Function that fethces the Reddit API
+    """
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
-    else:
-        print(None)
+    response = get(url, headers=user_agent, params=params)
+    if response.status_code != 200:
+        print("None")
+        return
+
+    try:
+        results = response.json()
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
